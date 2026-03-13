d5642f24363ffa34c6b9b640a2f6ff5d546b8d13
stella claire <scluttrell@comcast.net>
2026-03-13 13:55:39 -0500

    Modify URL patterns for email verification and add cart/checkout
    Updated the email verification path and added cart and checkout paths.

--- General/urls.py
+++ General/urls.py
@@ -5,9 +5,11 @@ urlpatterns = [
     path("", views.home, name="general_home"),
     path("login/", views.login_page, name="login"),
     path("register/", views.register, name="register"),
-    path("email-verification/", views.email_verif, name="email_verif"),
+    path("email-verification/", views.email_verification, name="email_verif"),
     path("catalog/", views.catalog, name="catalog"),
     path("catalog/<int:pk>/", views.book_detail, name="book_detail"),
     path("book_covers/<path:path>", views.serve_book_cover, name="serve_book_cover"),
+    path("cart/", views.cart, name="cart"),
+    path("checkout/", views.checkout, name="checkout"),
     path("logout/", views.logout_view, name="logout"),
 ]
