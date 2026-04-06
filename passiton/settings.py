bb2cde7d71aeaee98760219694cacba4c643dfa6
Athish Ram Das <athishr88@gmail.com>
2026-04-06 11:38:37 -0500

    nginx file error
    Server fails to load resources

--- passiton/settings.py
+++ passiton/settings.py
@@ -128,7 +128,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = "/static/"
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 # Serve book cover images from book_images/ (project root or parent directory)
 _BOOK_IMAGES_DIR = BASE_DIR / 'book_images'
