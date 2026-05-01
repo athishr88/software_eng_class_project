8e76a633db1786e9b3e6a2c9bdfa3a6a32612a86
Chip Hardy <chiphardy1995@icloud.com>
2026-05-01 14:53:43 -0500

    added initial general fixtures
    added a fixture for a buyer and seller as well as their addresses. Allows for testing of buyer and seller functionallity

---
+++ General/conftest.py
@@ -0,0 +1,46 @@
+import pytest
+from General.models import User, Address
+
+@pytest.fixture
+def example_buyer():
+    return User.objects.create(
+        role='Buyer',
+        email='buyer_email@testemail.com',
+        password='testpassword',
+        first_name='Buyer',
+        last_name='Test',
+    )
+
+@pytest.fixture
+def example_seller():
+    return User.objects.create(
+        role='Buyer',
+        email='buyer_email@testemail.com',
+        password='testpassword',
+        first_name='Buyer',
+        last_name='Test',
+    )
+
+@pytest.fixture
+def buyer_address(example_buyer):
+    return Address.objects.create(
+        user=example_buyer,
+        line1='1204 Example Street',
+        line2='Example apt',
+        city='Example City',
+        state='EX',
+        postal_code='12345',
+        country='US',
+    )
+
+@pytest.fixture
+def seller_address(example_seller):
+    return Address.objects.create(
+        user=example_seller,
+        line1='1204 Example Street',
+        line2='Example apt',
+        city='Example City',
+        state='EX',
+        postal_code='12345',
+        country='US',
+    )
