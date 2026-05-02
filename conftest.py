5bab93e5f952e03790acc7949868c01d9c3ad50d
Chip Hardy <chiphardy1995@icloud.com>
2026-05-01 22:22:37 -0500

    added inital setup for selenium
    added selenium and all of its dependancies to requirements.txt. Added a fixture to initalize the selenium webdriver.

--- conftest.py
+++ conftest.py
@@ -1,5 +1,15 @@
 import pytest
 from General.models import User, Address
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+
+@pytest.fixture
+def browser():
+    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+    driver.implicitly_wait(5)
+    yield driver 
+    driver.quit()
 
 @pytest.fixture
 def example_buyer():
